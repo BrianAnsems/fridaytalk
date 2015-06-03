5d0f016a00586b05f856b3ce024ff3f4596d4ce2
BrianAnsems <brianansems@gmail.com>
2015-06-03 16:23:29 -0400

    Lecture Complete Page
    Now the session menu on the left reloads the right panel to show a
    single lecture with its complete summary. Comments and author bio need
    to be added.

--- javascripts/fridaytalk.js
+++ javascripts/fridaytalk.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
 		collapsible:true,
 		active:false
 	});
+
 });
+function lecturePage(){
+		document.getElementById("lectures").innerHTML="";
+		$("#lectures").append('<div class="episodes"></div>');
+		$(".episodes").append('<div class="slideshare"></div>');
+		$(".slideshare").append('<iframe src="http://www.slideshare.net/slideshow/embed_code/key/MAB4Va88G7cwK4" width="425" height="355" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>');
+		$(".episodes").append('<div class="summary"></div>');
+		$(".summary").append('<h2>Full Lecture Summary</h2><p>The full summary of the lecture will go here.</p>');
+		$(".episodes").append('<div class="authorimage"></div>');
+		$(".authorimage").append('<img src="http://upload.wikimedia.org/wikipedia/commons/8/81/Dilma_Rousseff_-_foto_oficial_2011-01-09.jpg">');
+	}
