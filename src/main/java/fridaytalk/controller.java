86398c2ee48a63e166d80d695cacb71b38aa2d66
BrianAnsems <brianansems@gmail.com>
2015-06-18 11:16:46 -0400

    SpringBoot
    Trying to make website into Spring Application.

---
+++ src/main/java/fridaytalk/controller.java
@@ -0,0 +1,16 @@
+package fridaytalk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+
+public class controller{
+
+	@RequestMapping(value="fridaytalk", method=RequestMethod.GET)
+	public String mainPage(){
+		return "index";
+	}
+}
