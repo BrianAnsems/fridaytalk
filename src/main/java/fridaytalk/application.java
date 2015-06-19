728c77bf8306e20efb203009a536c00ac64cb3d4
BrianAnsems <brianansems@gmail.com>
2015-06-19 15:44:55 -0400

    Trying to match Example
    Changed pom.xml and application.java to resemble example.

--- src/main/java/fridaytalk/application.java
+++ src/main/java/fridaytalk/application.java
@@ -2,9 +2,16 @@ package fridaytalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class application{
+public class application extends SpringBootServletInitializer{
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(application.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(application.class, args);
