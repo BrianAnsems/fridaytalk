1aa9486efdedd0d7083fd72c68b81ab2a7d392b0
BrianAnsems <brianansems@gmail.com>
2015-06-23 12:54:14 -0400

    moved config file to java files
    Moved config file to java files.

--- src/main/java/fridaytalk/controller.java
+++ src/main/java/fridaytalk/controller.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+
 @Controller
 
 public class controller{
 
 	@RequestMapping(value="/fridaytalk", method=RequestMethod.GET)
 	public String mainPage(){
-		return "index";
+		return "test";
 	}
 }
